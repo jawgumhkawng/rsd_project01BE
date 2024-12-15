e1b0cd902507cf8f6d0a2557e96834c0d2ee4f66
jawgumhkawng <nawbosco007@gmail.com>
2024-12-15 11:35:39 +0630

    bug fix

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
+// npm i express cors body-parser nodemon
+
 const express = require("express");
 const app = express();
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const cors = require("cors");
+app.use(cors());
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-app.get('/posts', async(req, res) => {
+app.get('/posts', async (req, res) => {
     const posts = await prisma.post.findMany({
         take: 20,
-        orderBy: { id: 'desc' }
-    });
+        orderBy: { id: "desc" },
+        include: { user: true },
+    }) 
+
     res.json(posts);
 });
 
-app.get('/posts/:id', async(req, res) => {
+app.get('/posts/:id', async (req, res) => {
     const { id } = req.params;
     const post = await prisma.post.findUnique({
-        where: { id: Number(id) },
-    });
+        where: {
+            id: Number(id)
+        }
+    })
 
     res.json(post);
 });
 
-app.post('/posts', async (req, res) => {
+app.post("/posts", async (req, res) => {
     const { content } = req.body;
-    if (!content) {
-        return res.status(400).json({ msg: 'content is required' });
+    if(!content) {
+        return res.status(400).json({ msg: "content is required" })
     }
 
     const post = await prisma.post.create({
         data: {
             content,
-            userId: 1
+            userId: 1,
         },
-    });
+        include: { user: true }
+    })
 
     res.status(201).json(post);
-});
+})
 
-app.delete('/posts/:id', async (req, res) => {
-    const { id } = ewq.params;
+app.delete("/posts/:id", async (req, res) => {
+    const { id } = req.params;
 
     const post = await prisma.post.delete({
-        where: { is: Number(id) }
-    });
-    res.json(post);
-});
+        where: {
+            id: Number(id)
+        }
+    })
 
-app.listen(8080, () => {
+    setTimeout(() => {
+        res.json(post);
+    }, 2000);
+})
+
+app.listen(8000, () => {
+    // npx nodemon index.js
     console.log("Express API running at 8080");
 });
-
